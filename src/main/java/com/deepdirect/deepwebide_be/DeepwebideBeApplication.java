8ee248a40f6d1d25543b8b66433d3a8516ee33ba
projectmiluju <dnjsdyd0712@naver.com>
2025-07-18 09:06:12 +0900

    chore(setup): 프로젝트 생성

---
+++ src/main/java/com/deepdirect/deepwebide_be/DeepwebideBeApplication.java
@@ -0,0 +1,13 @@
+package com.deepdirect.deepwebide_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepwebideBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepwebideBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepdirect/deepwebide_be/DeepwebideBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepdirect.deepwebide_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepwebideBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
