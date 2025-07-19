315a223983819781e94a19fba8912c14a5bb8f00
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-19 22:43:59 +0900

    feat: 닉네임 중복 방지를 위해 추가 DP-39

---
+++ src/main/java/com/deepdirect/deepwebide_be/member/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.deepdirect.deepwebide_be.member.repository;
+
+import com.deepdirect.deepwebide_be.member.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 닉네임 중복 확인
+    boolean existsByNickname(String nickname);
+
+    // 닉네임 중복
+    @Query("SELECT u.nickname FROM User u WHERE u.nickname LIKE CONCAT(:base, '%')")
+    List<String> findNicknamesByPrefix(@Param("base") String baseNickname);
+}
