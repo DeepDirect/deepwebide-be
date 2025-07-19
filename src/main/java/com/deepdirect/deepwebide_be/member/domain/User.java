5cfc2f135e9cb5d3dd43ffc332e9a738085d8796
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-19 15:34:59 +0900

    feat: DP-37 Domain에 User 추가

---
+++ src/main/java/com/deepdirect/deepwebide_be/member/domain/User.java
@@ -0,0 +1,63 @@
+package com.deepdirect.deepwebide_be.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "nickname"),
+        @UniqueConstraint(columnNames = "email"),
+        @UniqueConstraint(columnNames = "phoneNumber")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // PK
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String profileImageUrl;
+
+    @Column(nullable = false)
+    private boolean emailVerified = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    /**
+     * 회원가입
+     */
+    public User(String username, String nickname, String email, String phoneNumber, String password, String profileImageUrl) {
+        this.username = username;
+        this.nickname = nickname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
