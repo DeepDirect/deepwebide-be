f78eeab54adf41d2d25187d9d6b526759dbad81e
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-21 10:13:16 +0900

    feat(SignIn): SignInResponse에 @AllArgsConstructor 추가 DP-39

--- src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
@@ -1,8 +1,10 @@
 package com.deepdirect.deepwebide_be.member.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SignInResponse {
     private String accessToken;
     private SignInUserDto user;
