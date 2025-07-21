78f7ce607838328fb1cddb65494450f57db12bab
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-21 10:35:58 +0900

    fix(SignIn): dto 스키마 추가 DP-39

--- src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
@@ -1,11 +1,17 @@
 package com.deepdirect.deepwebide_be.member.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@Schema
 @AllArgsConstructor
 public class SignInResponse {
+
+    @Schema(description = "Access Token (JWT)", example =  "eyJhbGciOi...")
     private String accessToken;
+
+    @Schema(description = "로그인 사용자 정보")
     private SignInUserDto user;
 }
--- src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInUserDto.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInUserDto.java
@@ -1,14 +1,26 @@
 package com.deepdirect.deepwebide_be.member.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import com.deepdirect.deepwebide_be.member.domain.User;
 import lombok.Getter;
 
 @Getter
+@Schema
 public class SignInUserDto {
+
+    @Schema(description = "사용자 id", example = "1")
     private Long id;
+
+    @Schema(description = "사용자 실명", example = "홍길동")
     private String username;
+
+    @Schema(description = "이메일", example = "test@test.com")
     private String email;
+
+    @Schema(description = "닉네임", example = "고통스러운 개발자")
     private String nickname;
+
+    @Schema(description = "프로필 이미지 URL", example = "https://cdn.pixabay.com/photo/2013/07/12/14/15/boy-148071_1280.png")
     private String profileImageUrl;
 
     public SignInUserDto(User user) {
