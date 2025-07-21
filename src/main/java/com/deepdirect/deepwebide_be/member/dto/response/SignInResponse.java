294c0652136a91311783a77aca9ad2ef6ff846d3
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-21 10:03:20 +0900

    feat(SignIn): dto에 @Builder 제거 DP-39

--- src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInResponse.java
@@ -1,10 +1,8 @@
 package com.deepdirect.deepwebide_be.member.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class SignInResponse {
     private String accessToken;
     private SignInUserDto user;
--- src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInUserDto.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/dto/response/SignInUserDto.java
@@ -1,10 +1,8 @@
 package com.deepdirect.deepwebide_be.member.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class SignInUserDto {
     private Long id;
     private String username;
