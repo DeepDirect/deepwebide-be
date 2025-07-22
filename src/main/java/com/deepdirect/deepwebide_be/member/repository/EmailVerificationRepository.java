a8b7a8e6d8d745fbc7e6aca691735f4688871f75
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-22 16:55:09 +0900

    feat(repository): 이메일 인증 리포지토리 EmailVerificationRepository 추가 DP-51

---
+++ src/main/java/com/deepdirect/deepwebide_be/member/repository/EmailVerificationRepository.java
@@ -0,0 +1,18 @@
+package com.deepdirect.deepwebide_be.member.repository;
+
+import com.deepdirect.deepwebide_be.member.domain.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+
+    // 이메일 조회
+    Optional<EmailVerification> findByEmail(String email);
+
+    // 이메일 인증 코드 조회
+    Optional<EmailVerification> findByEmailCode(String code);
+
+    // 이메일 인증 여부 확인
+    boolean existsByEmailAndVerifiedIsTrue(String email);
+}
