3d712e32e4bb49056d91a561c838157a1c9ab07d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-23 01:45:30 +0900

    feat(controller): 인증번호 확인 추가 DP-51

--- src/main/java/com/deepdirect/deepwebide_be/member/controller/PhoneVerificationController.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/controller/PhoneVerificationController.java
@@ -2,7 +2,9 @@ package com.deepdirect.deepwebide_be.member.controller;
 
 import com.deepdirect.deepwebide_be.global.dto.ApiResponseDto;
 import com.deepdirect.deepwebide_be.member.dto.request.PhoneVerificationRequest;
+import com.deepdirect.deepwebide_be.member.dto.request.PhoneVerifyCodeRequest;
 import com.deepdirect.deepwebide_be.member.dto.response.PhoneVerificationResponse;
+import com.deepdirect.deepwebide_be.member.dto.response.PhoneVerifyCodeResponse;
 import com.deepdirect.deepwebide_be.member.dto.response.SignUpResponse;
 import com.deepdirect.deepwebide_be.member.service.PhoneVerificationService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,7 +30,7 @@ public class PhoneVerificationController {
     @Operation(
             summary = "전화번호 인증"
     )
-    @PostMapping("/verify-code")
+    @PostMapping("/send-code")
     public ResponseEntity<ApiResponseDto<PhoneVerificationResponse>> sendCode(
             @Valid @RequestBody PhoneVerificationRequest request) {
 
@@ -41,6 +43,20 @@ public class PhoneVerificationController {
         PhoneVerificationResponse expiresIn = new PhoneVerificationResponse(time);
 
         return ResponseEntity.ok(ApiResponseDto.of(200, "인증번호가 발송되었습니다.", expiresIn));
+    }
+
+    @Operation(
+            summary = "전화번호 인증번호 확인"
+    )
+    @PostMapping("/verify-code")
+    public ResponseEntity<ApiResponseDto<PhoneVerifyCodeResponse>> verifyCode(
+            @Valid @RequestBody PhoneVerifyCodeRequest  request) {
+
+        boolean verificationResult = phoneVerificationService.verifyCode(request.getPhoneNumber(), request.getCode());
+
+        PhoneVerifyCodeResponse verificationResponse = new PhoneVerifyCodeResponse(verificationResult);
+
+        return ResponseEntity.ok(ApiResponseDto.of(200, "인증에 성공했습니다.", verificationResponse));
     }
 
 }
