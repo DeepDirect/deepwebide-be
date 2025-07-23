a50db06b42e1ca703c8d42806354af6859979cbb
sunsetkk <khyejin9796@gmail.com>
2025-07-24 00:48:18 +0900

    fix: Repository 어노테이션 누락 추가

--- src/main/java/com/deepdirect/deepwebide_be/repository/repository/RepositoryMemberRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/repository/RepositoryMemberRepository.java
@@ -3,7 +3,9 @@ package com.deepdirect.deepwebide_be.repository.repository;
 
 import com.deepdirect.deepwebide_be.repository.domain.RepositoryMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryMemberRepository extends JpaRepository<RepositoryMember, Long> {
 
     boolean existsByRepositoryIdAndUserId(Long repositoryId, Long userId);
