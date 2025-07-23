bc095621a614833a29d8b48be9e2e03c69daef27
sunsetkk <khyejin9796@gmail.com>
2025-07-24 01:59:36 +0900

    feat(setting): 멤버 목록/존재 확인용 쿼리 메서드 추가 DP-98

--- src/main/java/com/deepdirect/deepwebide_be/repository/repository/RepositoryMemberRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/repository/RepositoryMemberRepository.java
@@ -4,11 +4,13 @@ package com.deepdirect.deepwebide_be.repository.repository;
 import com.deepdirect.deepwebide_be.repository.domain.RepositoryMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepositoryMemberRepository extends JpaRepository<RepositoryMember, Long> {
 
     boolean existsByRepositoryIdAndUserId(Long repositoryId, Long userId);
-    boolean existsByUserIdAndRepositoryId(Long userId, Long repositoryId);
-
     long countByRepositoryIdAndDeletedAtIsNull(Long repositoryId);
     void deleteByUserIdAndRepositoryId(Long userId, Long repositoryId);
+
+    List<RepositoryMember> findAllByRepositoryId(Long repositoryId);
 }
