e63d9fee6dc2e73fb46d4a936be730c62fadc401
sunsetkk <khyejin9796@gmail.com>
2025-07-23 15:43:39 +0900

    feat(repository): RepositoryMemberRepository 신규 생성 및 참여자 중복 조회 기능 추가 DP-59

---
+++ src/main/java/com/deepdirect/deepwebide_be/repository/repository/RepositoryMemberRepository.java
@@ -0,0 +1,10 @@
+package com.deepdirect.deepwebide_be.repository.repository;
+
+
+import com.deepdirect.deepwebide_be.repository.domain.RepositoryMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryMemberRepository extends JpaRepository<RepositoryMember, Long> {
+
+    boolean existsByRepositoryIdAndUserId(Long repositoryId, Long userId);
+}
