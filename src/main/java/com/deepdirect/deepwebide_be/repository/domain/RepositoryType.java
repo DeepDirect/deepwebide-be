f67f8314cd6ccd0d2778fec6ffeefc4efaa37679
sunsetkk <khyejin9796@gmail.com>
2025-07-24 09:16:52 +0900

    refacor(repo): 상수 네이밍 통일(대문자+스네이크) DP-99

--- src/main/java/com/deepdirect/deepwebide_be/repository/domain/RepositoryType.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/domain/RepositoryType.java
@@ -1,6 +1,5 @@
 package com.deepdirect.deepwebide_be.repository.domain;
 
 public enum RepositoryType {
-    // TODO: enum 값 확정 필요
-    SPRING_BOOT, REACT, FastAPI
+    SPRING_BOOT, REACT, FAST_API
 }
