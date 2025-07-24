da0387b8b631e0a2e362bf080d0e6491546e582b
sunsetkk <khyejin9796@gmail.com>
2025-07-24 12:33:01 +0900

    refactor(user): 사용되지 않는 import 삭제 DP-103

--- src/main/java/com/deepdirect/deepwebide_be/member/repository/PhoneVerificationRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/member/repository/PhoneVerificationRepository.java
@@ -2,9 +2,7 @@ package com.deepdirect.deepwebide_be.member.repository;
 
 import com.deepdirect.deepwebide_be.member.domain.AuthType;
 import com.deepdirect.deepwebide_be.member.domain.PhoneVerification;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
