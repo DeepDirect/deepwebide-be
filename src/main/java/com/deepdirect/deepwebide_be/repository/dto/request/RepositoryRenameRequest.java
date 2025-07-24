7282ac3701615b71ade7e70ab342d56ef4503039
sunsetkk <khyejin9796@gmail.com>
2025-07-24 09:59:02 +0900

    refacor(repo): DTO 스키마 작성  DP-99

--- src/main/java/com/deepdirect/deepwebide_be/repository/dto/request/RepositoryRenameRequest.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/dto/request/RepositoryRenameRequest.java
@@ -1,13 +1,16 @@
 package com.deepdirect.deepwebide_be.repository.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "레포지토리 이름 변경 요청 DTO")
 public class RepositoryRenameRequest {
 
     @NotBlank
     @Size(max = 50, message = "레포지토리 이름은 50자 이하여야 합니다.")
+    @Schema(description = "레포지토리 이름", example = "deepwebide-refactored")
     private String repositoryName;
 }
--- src/main/java/com/deepdirect/deepwebide_be/repository/dto/response/RepositoryListResponse.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/dto/response/RepositoryListResponse.java
@@ -1,5 +1,6 @@
 package com.deepdirect.deepwebide_be.repository.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,11 +8,21 @@ import java.util.List;
 
 @Getter
 @Builder
+@Schema(description = "레포지토리 리스트 조회 응답 DTO")
 public class RepositoryListResponse {
 
+    @Schema(description = "현재 페이지 번호", example = "0")
     private int currentPage;
+
+    @Schema(description = "페이지당 레포지토리 개수", example = "7")
     private int pageSize;
+
+    @Schema(description = "전체 페이지 수", example = "5")
     private int totalPages;
+
+    @Schema(description = "전체 레포지토리 수", example = "32")
     private long totalElements;
+
+    @Schema(description = "레포지토리 응답 목록")
     private List<RepositoryResponse> repositories;
 }
