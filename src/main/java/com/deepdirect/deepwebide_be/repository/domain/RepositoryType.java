91b416b026b3dd942dd226995e7fac4e669a1e75
sunsetkk <khyejin9796@gmail.com>
2025-07-27 00:45:26 +0900

    refec(repo): swagger 문서화 enum 설명 추가 DP-126

--- src/main/java/com/deepdirect/deepwebide_be/repository/domain/RepositoryType.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/domain/RepositoryType.java
@@ -1,5 +1,8 @@
 package com.deepdirect.deepwebide_be.repository.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "레포지토리의 개발 환경 타입")
 public enum RepositoryType {
     SPRING_BOOT, REACT, FAST_API
 }
