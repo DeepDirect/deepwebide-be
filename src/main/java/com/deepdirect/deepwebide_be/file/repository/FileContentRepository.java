2edf57490c584d4db9996cd29214d1541f3d3e7f
projectmiluju <dnjsdyd0712@naver.com>
2025-07-28 08:45:50 +0900

    feat(file): FileNode로 FileContent를 조회하는 메서드 추가 DP-128

--- src/main/java/com/deepdirect/deepwebide_be/file/repository/FileContentRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/file/repository/FileContentRepository.java
@@ -4,7 +4,12 @@ import com.deepdirect.deepwebide_be.file.domain.FileContent;
 import com.deepdirect.deepwebide_be.file.domain.FileNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileContentRepository extends JpaRepository<FileContent, Long> {
 
     void deleteByFileNode(FileNode fileNode);
+
+    Optional<FileContent> findByFileNode(FileNode fileNode);
+
 }
