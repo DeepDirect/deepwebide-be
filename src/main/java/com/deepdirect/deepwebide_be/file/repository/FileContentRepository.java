6128a27c270a1b25f1d45e0abad530020a17b9d8
projectmiluju <dnjsdyd0712@naver.com>
2025-07-28 05:55:06 +0900

    feat(file): 파일 노드에 따른 삭제 메서드 추가 DP-128

--- src/main/java/com/deepdirect/deepwebide_be/file/repository/FileContentRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/file/repository/FileContentRepository.java
@@ -1,8 +1,10 @@
 package com.deepdirect.deepwebide_be.file.repository;
 
 import com.deepdirect.deepwebide_be.file.domain.FileContent;
+import com.deepdirect.deepwebide_be.file.domain.FileNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileContentRepository extends JpaRepository<FileContent, Long> {
-    // 보통 fileNode로 조회, 필요에 따라 메서드 추가 가능
+
+    void deleteByFileNode(FileNode fileNode);
 }
