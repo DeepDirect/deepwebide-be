4e8089f04715939fc72f0b120eb93d9b1460739a
projectmiluju <dnjsdyd0712@naver.com>
2025-07-28 05:51:35 +0900

    refactor(file): 사용하지 않는 import문 제거 DP-128

--- src/main/java/com/deepdirect/deepwebide_be/file/repository/FileNodeRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/file/repository/FileNodeRepository.java
@@ -3,13 +3,9 @@ package com.deepdirect.deepwebide_be.file.repository;
 import com.deepdirect.deepwebide_be.file.domain.FileNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.List;
 
 public interface FileNodeRepository extends JpaRepository<FileNode, Long> {
-
-    // 경로+레포로 파일/폴더 노드 찾기 (중복생성 방지용)
-    Optional<FileNode> findByRepositoryIdAndPath(Long repositoryId, String path);
 
     // 특정 레포의 모든 파일/폴더 조회
     List<FileNode> findAllByRepositoryId(Long repositoryId);
