e35c9bf902199a4c6319b2bb08650356a016767b
projectmiluju <dnjsdyd0712@naver.com>
2025-07-28 14:45:58 +0900

    refactor(file): Redis 서비스에서 파일 내용 저장 및 조회 메서드 개선 DP-128

--- src/main/java/com/deepdirect/deepwebide_be/file/service/FileContentRedisService.java
+++ src/main/java/com/deepdirect/deepwebide_be/file/service/FileContentRedisService.java
@@ -1,26 +1,25 @@
 package com.deepdirect.deepwebide_be.file.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FileContentRedisService {
-    private final StringRedisTemplate redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    private static final String REDIS_FILE_CONTENT_PREFIX = "file-content:";
-
-    // 저장
-    public void saveFileContentToRedis(Long repositoryId, Long fileId, String content) {
-        String key = REDIS_FILE_CONTENT_PREFIX + repositoryId + ":" + fileId;
+    public void saveFileContent(Long repositoryId, Long fileId, String content) {
+        String key = getFileContentKey(repositoryId, fileId);
         redisTemplate.opsForValue().set(key, content);
     }
 
-    // 조회
-    public String getFileContentFromRedis(Long repositoryId, Long fileId) {
-        String key = REDIS_FILE_CONTENT_PREFIX + repositoryId + ":" + fileId;
+    public String getFileContent(Long repositoryId, Long fileId) {
+        String key = getFileContentKey(repositoryId, fileId);
         return redisTemplate.opsForValue().get(key);
     }
 
+    private String getFileContentKey(Long repositoryId, Long fileId) {
+        return "file-content:" + repositoryId + ":" + fileId;
+    }
 }
