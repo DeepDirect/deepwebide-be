2f99ff55d9105a1f027a1fc14230d0b5c78dcd32
sunsetkk <khyejin9796@gmail.com>
2025-07-28 12:59:20 +0900

    feature(chat): 채팅 메시지 조회 서비스 로직 구현 DP-134

--- src/main/java/com/deepdirect/deepwebide_be/chat/repository/ChatMessageReferenceRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/chat/repository/ChatMessageReferenceRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatMessageReferenceRepository extends  JpaRepository<ChatMessageReference, Long> {
+    //조회된 메세지 목록 기준으로 관련된 참조 정보 한번에 조회
     List<ChatMessageReference> findByChatMessageIdIn(List<Long> chatMessageIds);
 }
