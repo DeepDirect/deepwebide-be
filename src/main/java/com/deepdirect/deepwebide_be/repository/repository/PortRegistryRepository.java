956405319a80c96bbdd39ca7e5ad98c1011d436e
projectmiluju <dnjsdyd0712@naver.com>
2025-07-29 12:14:33 +0900

    feat(repository): Repository와 status로 PortRegistry 찾는 메서드 추가 DP-73

--- src/main/java/com/deepdirect/deepwebide_be/repository/repository/PortRegistryRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/repository/PortRegistryRepository.java
@@ -2,10 +2,15 @@ package com.deepdirect.deepwebide_be.repository.repository;
 
 import com.deepdirect.deepwebide_be.repository.domain.PortRegistry;
 import com.deepdirect.deepwebide_be.repository.domain.PortStatus;
+import com.deepdirect.deepwebide_be.repository.domain.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PortRegistryRepository extends JpaRepository<PortRegistry, Long> {
+
     Optional<PortRegistry> findFirstByStatusOrderByPortAsc(PortStatus status);
+
+    Optional<PortRegistry> findByRepository(Repository repository);
+    Optional<PortRegistry> findFirstByStatus(PortStatus status);
 }
