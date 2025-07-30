11ca39c07bec9c2fceb37b499d64a6eda15ba497
sunsetkk <khyejin9796@gmail.com>
2025-07-31 01:39:35 +0900

    feature(chat): ChatReadOffsetRepository 생성 DP-157

--- src/main/java/com/deepdirect/deepwebide_be/chat/repository/ChatReadOffsetRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/chat/repository/ChatReadOffsetRepository.java
@@ -1,2 +1,11 @@
-package com.deepdirect.deepwebide_be.chat.repository;public class ChatReadOffsetRepository {
+package com.deepdirect.deepwebide_be.chat.repository;
+
+import com.deepdirect.deepwebide_be.chat.domain.ChatReadOffset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ChatReadOffsetRepository extends JpaRepository<ChatReadOffset, Long> {
+
+    Optional<ChatReadOffset> findByRepositoryIdAndUserId(Long repositoryId, Long userId);
 }
