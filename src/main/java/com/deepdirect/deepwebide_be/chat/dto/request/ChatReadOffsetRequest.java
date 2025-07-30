0b224de02daf0a70a99850ce32b7f23212f031d4
sunsetkk <khyejin9796@gmail.com>
2025-07-31 01:40:43 +0900

    feature(chat): 읽음 처리 요청 DTO 추가 및 수정 DP-157

--- src/main/java/com/deepdirect/deepwebide_be/chat/dto/request/ChatReadOffsetRequest.java
+++ src/main/java/com/deepdirect/deepwebide_be/chat/dto/request/ChatReadOffsetRequest.java
@@ -1,2 +1,22 @@
-package com.deepdirect.deepwebide_be.chat.dto.request;public class ChatReadOffsetRequest {
+package com.deepdirect.deepwebide_be.chat.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "채팅 읽음 위치 저장 요청")
+public class ChatReadOffsetRequest {
+
+    @NotNull
+    @Schema(description = "마지막으로 읽은 메시지 ID", example = "152")
+    private Long lastReadMessageId;
+
+    @Builder
+    public ChatReadOffsetRequest(Long lastReadMessageId) {
+        this.lastReadMessageId = lastReadMessageId;
+    }
 }
