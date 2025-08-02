c60592aacd867cc857eebb5c87ab5cd98e9d801a
projectmiluju <dnjsdyd0712@naver.com>
2025-08-03 02:14:50 +0900

    feat(RepositoryRunService): 컨테이너 자동 종료 기능 추가 DP-200

--- src/main/java/com/deepdirect/deepwebide_be/repository/repository/PortRegistryRepository.java
+++ src/main/java/com/deepdirect/deepwebide_be/repository/repository/PortRegistryRepository.java
@@ -5,6 +5,7 @@ import com.deepdirect.deepwebide_be.repository.domain.PortStatus;
 import com.deepdirect.deepwebide_be.repository.domain.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PortRegistryRepository extends JpaRepository<PortRegistry, Long> {
@@ -13,4 +14,8 @@ public interface PortRegistryRepository extends JpaRepository<PortRegistry, Long
 
     Optional<PortRegistry> findByRepository(Repository repository);
     Optional<PortRegistry> findFirstByStatus(PortStatus status);
+
+    Optional<PortRegistry> findByPort(Integer port);
+    List<PortRegistry> findAllByStatus(PortStatus status);
+
 }
